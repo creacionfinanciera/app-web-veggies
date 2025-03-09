9a0a91043e568af5bb3ea2908c00eb38942e1e60
Luis Cantor <luiskentor@gmail.com>
2025-03-09 15:34:38 -0500

    Actualizando prueba.js

--- prueba.js
+++ prueba.js
@@ -2,4 +2,6 @@ function saludo () {
   console.log('Hola mundo desde VSC');
   console.log('Hola mundo desde GAS');
   console.log('Hola mundo proyecto clonado');
+  console.log('Hola mundo despues de clonar github');
+  console.log('Hola mundo despues de corregir gitignore');
 }
